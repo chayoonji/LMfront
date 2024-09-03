58f958f615429b890b0df7886a3a1bf50b2cb2e9
chayoonji <zizi2481@gmail.com>
2024-09-04 04:18:54 +0900

    프록시 추가

---
+++ setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'https://port-0-lmserver-7xwyjq992llj191csy.sel4.cloudtype.app/', // 백엔드 서버의 주소
+      changeOrigin: true,
+    })
+  );
+};
