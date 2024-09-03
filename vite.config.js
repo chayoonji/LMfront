77d4a11a1259c16bce5a7f2fd9c02aaf45183c3e
chayoonji <zizi2481@gmail.com>
2024-09-04 00:09:45 +0900

    포트수정

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: 3000, // 개발 모드에서 사용하는 기본 포트
+  },
+  preview: {
+    port: 4173, // Vite 프리뷰 모드에서 사용하는 기본 포트
+  },
+});
