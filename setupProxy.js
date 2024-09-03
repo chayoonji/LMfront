7e438430a0bc7c5fcb2c4be5e86400c0d724c781
chayoonji <zizi2481@gmail.com>
2024-09-04 04:20:24 +0900

    슬레시제거

--- setupProxy.js
+++ setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'https://port-0-lmserver-7xwyjq992llj191csy.sel4.cloudtype.app/', // 백엔드 서버의 주소
+      target: 'https://port-0-lmserver-7xwyjq992llj191csy.sel4.cloudtype.app', // 백엔드 서버의 주소
       changeOrigin: true,
     })
   );
